a4a23d01009a3326164abce7df1a23a72c62e4d2
PradakshinaRB <rbpradakshina@gmail.com>
2025-07-30 14:46:02 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i=50;i<=100;i++)
+		{
+			sum+=i;
+		}
+		System.out.println("sum of numbers from 50 to 1060 is:"+sum);
+	}
+
+}
