349d7af1908cd2583cbf322f252d80c86eb4cb6e
PradakshinaRB <rbpradakshina@gmail.com>
2025-08-27 13:48:58 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,1 +1,42 @@
+package com.example.polymorphism;
 
+public class Admin extends User {
+	 @Override
+	 public void login() {
+		 System.out.println("Admin logged in with admin privileges.");
+	    }
+}
+
+package com.example.polymorphism;
+
+public class Customer extends User {
+	 @Override
+	 public void login() {
+		 System.out.println("Customer logged in with limited access.");
+	 }
+
+}
+package com.example.polymorphism;
+
+public class PolymorphismDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pradakshina RB");
+		System.out.println("2117240070224");
+		User user1=new Admin();
+		User user2=new Customer();
+		user1.login();
+		user2.login();
+
+	}
+
+}
+package com.example.polymorphism;
+
+public class User {
+	public void login() {
+        System.out.println("User logging in...");
+    }
+
+}
